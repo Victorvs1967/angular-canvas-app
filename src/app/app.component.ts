46bcecf5906375753d8e4e106e67a637511bd7d0
Victorvs1967 <victorsmirnov@me.com>
2024-01-14 12:14:03 +0200

    add canvas lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import confetti from 'canvas-confetti';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +13,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-canvas-app';
+
+  celebrate() {
+    const duration = 5000;
+
+    confetti({
+      particleCount: 100,
+      spread: 160,
+      origin: { y: 1 },
+      colors: ['#FF4500', '#008080', '#FFD700']
+    });
+
+    setTimeout(() => confetti.reset(), duration);
+  }
 }
